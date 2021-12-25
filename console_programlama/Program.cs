e709ff85cc1a559eef05f11db411cb5686999170
ipek <ipek_serttas99@htomail.com>
2021-12-25 16:06:11 +0300

    first commit

---
+++ console_programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba "+name+" "+surname);
+        }
+    }
+}

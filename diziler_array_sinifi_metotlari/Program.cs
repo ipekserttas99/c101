a2ed657f3cb90076728e30c587626a3c00dce795
ipek <ipek_serttas99@htomail.com>
2021-12-28 20:02:14 +0300

    array sınıfı metotları eklendi

---
+++ diziler_array_sinifi_metotlari/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metotlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+
+            Console.WriteLine("**** Sırasız Dizi ****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+
+            Console.WriteLine("*** Sıralı Dizi ****");
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Clear
+            Console.WriteLine("***  Array Clear  ****");
+            //sayiDizisi elemanlarını kullanarak 2. index ten itibaren 2 tane elemanı 0'lar
+            Array.Clear(sayiDizisi,2,2);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Reverse
+            Console.WriteLine("***  Array Reverse  ****");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //IndexOf
+            Console.WriteLine("***  Array IndexOf  ****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+
+            //Resize
+             Console.WriteLine("***  Array Resize  ****");
+             Array.Resize<int>(ref sayiDizisi,9);
+             sayiDizisi[8] = 99;
+
+             foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
